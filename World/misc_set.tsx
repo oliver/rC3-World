dd71e2ed30b165060b2e4723224a4c97d2030171
azriel423 <github@az-riel.de>
2020-12-20 20:27:58 +0100

    First start on the world

---
+++ World/misc_set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="misc_set" tilewidth="32" tileheight="32" tilecount="52" columns="2">
+ <image source="../Other_Tilesets/misc_set.png" width="64" height="832"/>
+</tileset>
