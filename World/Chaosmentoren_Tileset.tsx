a6f0f4ab917a94581792c6608aa863cf42fcb5ff
azriel423 <github@az-riel.de>
2020-12-26 20:30:44 +0100

    World-Update
    Update on the Chaospa:tinnen World

--- World/Chaosmentoren_Tileset.tsx
+++ World/Chaosmentoren_Tileset.tsx
@@ -1,4 +1,68 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="Chaosmentoren_Tileset" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
  <image source="../Chaospatinnen_Tileset/Chaosmentoren_Tileset.png" width="1024" height="1024"/>
+ <tile id="224">
+  <animation>
+   <frame tileid="192" duration="600"/>
+   <frame tileid="200" duration="600"/>
+   <frame tileid="208" duration="600"/>
+   <frame tileid="200" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="225">
+  <animation>
+   <frame tileid="193" duration="600"/>
+   <frame tileid="201" duration="600"/>
+   <frame tileid="209" duration="600"/>
+   <frame tileid="201" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="226">
+  <animation>
+   <frame tileid="194" duration="600"/>
+   <frame tileid="202" duration="600"/>
+   <frame tileid="210" duration="600"/>
+   <frame tileid="202" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="227">
+  <animation>
+   <frame tileid="195" duration="600"/>
+   <frame tileid="203" duration="600"/>
+   <frame tileid="211" duration="600"/>
+   <frame tileid="203" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="228">
+  <animation>
+   <frame tileid="196" duration="600"/>
+   <frame tileid="204" duration="600"/>
+   <frame tileid="212" duration="600"/>
+   <frame tileid="204" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="229">
+  <animation>
+   <frame tileid="197" duration="600"/>
+   <frame tileid="205" duration="600"/>
+   <frame tileid="213" duration="600"/>
+   <frame tileid="205" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="230">
+  <animation>
+   <frame tileid="198" duration="600"/>
+   <frame tileid="206" duration="600"/>
+   <frame tileid="214" duration="600"/>
+   <frame tileid="206" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="231">
+  <animation>
+   <frame tileid="199" duration="600"/>
+   <frame tileid="207" duration="600"/>
+   <frame tileid="215" duration="600"/>
+   <frame tileid="207" duration="600"/>
+  </animation>
+ </tile>
 </tileset>
