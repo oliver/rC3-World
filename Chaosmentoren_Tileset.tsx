ec96289178c1bb5d65994d9cbe520952e025108a
azriel423 <github@az-riel.de>
2020-12-27 15:34:01 +0100

    Fixing of all the tileset-references

--- Chaosmentoren_Tileset.tsx
+++ Chaosmentoren_Tileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="Chaosmentoren_Tileset" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
- <image source="../Chaospatinnen_Tileset/Chaosmentoren_Tileset.png" width="1024" height="1024"/>
+ <image source="Chaospatinnen_Tileset/Chaosmentoren_Tileset.png" width="1024" height="1024"/>
  <tile id="224">
   <animation>
    <frame tileid="192" duration="600"/>
--- mapUtilies.tsx
+++ mapUtilies.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="mapUtilies" tilewidth="32" tileheight="32" tilecount="9" columns="9">
- <image source="../Other_Tilesets/mapUtilities.png" width="288" height="32"/>
+ <image source="Other_Tilesets/mapUtilities.png" width="288" height="32"/>
 </tileset>
